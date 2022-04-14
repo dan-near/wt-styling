1b781049c8b3607ae0930d690761faf4a20beefc
Jacob <encody@users.noreply.github.com>
2022-04-14 13:33:08 -0500

    rough skeleton

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,8 +23,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/NEAR-Edu/welcome-track/tree/main/',
         },
         blog: {
           showReadingTime: true,
---
+++ src/pages/build-with-react.md
@@ -0,0 +1,7 @@
+---
+title: Build dApps with React
+---
+
+# Build dApps with React
+
+Transact, onboard, and manage identity all in a server-less environment. Censorship resistant with guaranteed uptime and verifiable execution, NEAR provides the best possible developer experience.
