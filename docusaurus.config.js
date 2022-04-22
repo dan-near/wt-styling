0177b1a83f8b5fd9fc715160b0d7fc7e64218717
Petar Vujović <petarvujovic98@gmail.com>
2022-04-22 20:24:48 +0200

    Custom style changes from Dan

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,12 +7,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'NEAR Welcome Track',
-  tagline: 'The NEAR Welcome Track is a guide for developers and entrepreneurs to get started with NEAR',
-  url: 'https://near.org',
+  tagline:
+    'The NEAR Welcome Track is a guide for developers and entrepreneurs to get started in Web3 with NEAR',
+  url: 'https://near.university',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/near-favicon.png',
   organizationName: 'NEAR-Edu', // Usually your GitHub org/user name.
   projectName: 'welcome-track', // Usually your repo name.
 
@@ -42,70 +43,115 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'NEAR University',
+          src: 'img/nu_logo.svg',
+          srcDark: 'img/nu_logo_white.svg',
+          href: 'https://www.near.university/',
         },
         items: [
           {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'NEAR for React devs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          /*{
+            to: '/blog', 
+            label: 'Blog', 
+            position: 'left'
+          },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
             position: 'right',
-          },
+          },*/
         ],
       },
       footer: {
-        style: 'dark',
+        style: 'light',
         links: [
           {
-            title: 'Docs',
+            title: 'Explore',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'NEAR Official Documentation',
+                href: 'https://docs.near.org/',
+              },
+              {
+                label: 'NEAR Official Wiki',
+                href: 'https://wiki.near.org/',
+              },
+              {
+                label: 'NEAR Protocol Specification',
+                href: 'https://nomicon.io/',
+              },
+              {
+                label: 'Code Examples',
+                href: 'https://examples.near.org/',
+              },
+              {
+                label: 'Explore NEAR Protocol APIs',
+                href: 'http://bit.ly/near-apis',
               },
             ],
           },
           {
-            title: 'Community',
+            title: 'Learn',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: 'NEAR Certified Developer Course',
+                href: 'https://www.near.university/courses/near-certified-developer',
               },
               {
+                label: 'NEAR Academy',
+                href: 'https://near.academy/',
+              },
+              {
+                label: 'NEAR for Senior Developers',
+                href: 'https://hackmd.io/@nearly-learning/near-201',
+              },
+              {
+                label: 'NEAR 101',
+                href: 'http://bit.ly/near-101',
+              },
+              {
+                label: 'NEAR 102',
+                href: 'http://bit.ly/near-102',
+              },
+              {
+                label: 'Write your first smart contract',
+                href: 'https://github.com/near-examples/workshop--exploring-assemblyscript-contracts/tree/master/assembly/A.sample-projects/01.greeting',
+              },
+            ],
+          },
+          {
+            title: 'Stay connected',
+            items: [
+              {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                href: 'https://discord.gg/k4pxafjMWA',
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
+                href: 'https://twitter.com/NEARedu',
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: 'Instagram',
+                href: 'https://www.instagram.com/near.university/?hl=en',
+              },
+              {
+                label: 'YouTube',
+                href: 'https://www.youtube.com/c/NEARProtocol',
+              },
+              {
+                label: 'NEAR Education Official Website',
+                href: 'https://www.near.university/',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} NEAR University is the project of NEAR Education`,
       },
       prism: {
         theme: lightCodeTheme,
