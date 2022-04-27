83f08948b2e290f585f473653cc5d3d172782eb8
Jacob <encody@users.noreply.github.com>
2022-04-27 13:27:48 -0500

    pre-commit hooks (formatting)

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'all',
+  printWidth: 180,
+  bracketSpacing: true,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'always',
+};
